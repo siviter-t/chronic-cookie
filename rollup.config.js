168d3c647a6425d749fc764f0363ff1fbb5df08a
siviter-t <siviter-t@users.noreply.github.com>
2018-06-01 19:00:26 +0100

    init: project setup

---
+++ rollup.config.js
@@ -0,0 +1,8 @@
+export default {
+    input: 'build/components/chronicCookie.js',
+    output: {
+        name: 'ChronicCookie',
+        file: 'build/chronic-cookie.js',
+        format: 'iife'
+    }
+};

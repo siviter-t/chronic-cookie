316c722668e0e4873433acaa4cc9354d8d51125f
siviter-t <siviter-t@users.noreply.github.com>
2018-06-04 23:01:07 +0100

    feat: expose underlying classes to public

--- src/chronicCookie.ts
+++ src/chronicCookie.ts
@@ -1,7 +1,15 @@
 import _Cookie from "./cookie";
 import _CookieConsent from "./cookieConsent";
+
+import {
+    ConsentDialog as _ConsentDialog,
+    ConsentDialogPosition as _ConsentDialogPosition
+} from "./consentDialog";
+
 import _CookieManager from "./cookieManager";
 
 export const Cookie = _Cookie;
-export const CookieConsent = _CookieConsent;
 export const CookieManager = _CookieManager;
+export const CookieConsent = _CookieConsent;
+export const ConsentDialog = _ConsentDialog;
+export const ConsentDialogPosition = _ConsentDialogPosition;

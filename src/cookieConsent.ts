23b6dcd50349337781b3709760277159a7f490fd
siviter-t <siviter-t@users.noreply.github.com>
2018-06-04 23:00:11 +0100

    feat: add cookie consent manger

--- src/cookieConsent.ts
+++ src/cookieConsent.ts
@@ -1,55 +1,81 @@
 import Cookie from "./cookie";
 import CookieManager from "./cookieManager";
 import Logger from "./logger";
+import { ConsentDialog } from "./consentDialog";
 
 export default abstract class CookieConsent {
 
-    public static readonly defaultExpiryDurationInDays = 365;
     public static readonly key = `${Logger.applicationName}_consented`;
+    public static readonly optInValue = "1";    
+    public static readonly optOutValue = "0";    
+
+    public static defaultOptInExpiryDurationInDays = 365;
+    public static defaultOptOutExpiryDurationInDays = 1;
 
     /**
-     * Returns a value indicating whether cookie consent been previously given
+     * The callback function called if cookie consent has been given.
      */
-    public static check(): boolean {
-        return CookieManager.check(CookieConsent.key);
+    public static optInCallback?: () => void;
+
+    /**
+     * The callback function called if cookie consent has not been given.
+     */
+    public static optOutCallback?: () => void;
+
+    /**
+     * Returns a value indicating whether cookie consent been previously given or refused
+     * If consent has never been given (i.e., no cookie is set), returns null
+     */
+    public static check(): boolean | null {
+        let value = CookieManager.getValue(CookieConsent.key);
+        switch (value) {
+            case CookieConsent.optInValue: return true;
+            case CookieConsent.optOutValue: return false;
+            default: return null;
+        }
     }
 
     /**
-     * Sets the consent cookie on the client browser
+     * Sets the consent cookie to opt-in on the client browser
      */
-    public static set(): void {
-        let consentCookie = new Cookie(CookieConsent.key, "1");
-        consentCookie.expireInDays(CookieConsent.defaultExpiryDurationInDays);
+    public static optIn(): void {
+        let consentCookie = new Cookie(CookieConsent.key, CookieConsent.optInValue);
+        consentCookie.expireInDays(CookieConsent.defaultOptInExpiryDurationInDays);
         CookieManager.set(consentCookie);
+        if (CookieConsent.optInCallback) CookieConsent.optInCallback();
+        ConsentDialog.closeDialog();
+    }
+
+    /**
+     * Sets the consent cookie to opt-out on the client browser
+     */
+    public static optOut(): void {
+        let consentCookie = new Cookie(CookieConsent.key, CookieConsent.optOutValue);
+        consentCookie.expireInDays(CookieConsent.defaultOptOutExpiryDurationInDays);
+        CookieManager.set(consentCookie);
+        if (CookieConsent.optOutCallback) CookieConsent.optOutCallback();
+        ConsentDialog.closeDialog();
     }
 
     /**
      * Resets the consent cookie on the client browser
      */
     public static reset(): void {
-        CookieManager.delete(new Cookie(CookieConsent.key, "1"));
-    }
-
-    /**
-     * Ask the client for cookie consent and set if given
-     * @param method Callback that returns a value whether the client has consented
-     */
-    public static ask(method: () => boolean): boolean {
-        let askResult = method();
-        if (askResult) CookieConsent.set();
-        return askResult;
+        CookieManager.delete(new Cookie(CookieConsent.key));
     }
 
     /**
      * Shorthand method for checking, asking, and setting cookies based on client consent
-     * @param askMethod Callback that returns a value whether the client has consented
-     * @param cookieMethod Callback for setting any cookies based on consent
+     * @param cookieCallback Callback for setting any cookies if consent has been given
      */
-    public static go(askMethod: () => boolean, cookieMethod: () => void): void {
-        if (CookieConsent.check() || CookieConsent.ask(askMethod)) {
-            cookieMethod();
-        } else {
-            Logger.warn("Cookie consent has not been given. Be wary of unexpected behaviour!");
+    public static go(): void {
+        let check = CookieConsent.check();
+        if (check === null) {
+            ConsentDialog.showPassiveDialog();
+        } else if (check && CookieConsent.optInCallback) {
+            CookieConsent.optInCallback();
+        } else if (!check && CookieConsent.optOutCallback) {
+            CookieConsent.optOutCallback();
         }
     }
 }
